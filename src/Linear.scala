bed29c6441685e1415f630ce98e4b898d83712de
BankoRiLoSe <2089058985@qq.com>
2026-07-20 18:36:44 +0800

    feat(linear): parameterize data and accumulator widths

--- src/Linear.scala
+++ src/Linear.scala
@@ -1,15 +1,16 @@
 import chisel3._
 
-class Linear(inFeatures: Int, outFeatures: Int) extends Module {
+class Linear(inFeatures: Int, outFeatures: Int, dataWidth: Int = 16, accWidth: Int = 32)
+    extends Module {
 
   val io = IO(new Bundle {
-    val x = Input(Vec(inFeatures, SInt(16.W)))
-    val w = Input(Vec(outFeatures, Vec(inFeatures, SInt(16.W))))
-    val bias = Input(Vec(outFeatures, SInt(32.W)))
-    val y = Output(Vec(outFeatures, SInt(32.W)))
+    val x = Input(Vec(inFeatures, SInt(dataWidth.W)))
+    val w = Input(Vec(outFeatures, Vec(inFeatures, SInt(dataWidth.W))))
+    val bias = Input(Vec(outFeatures, SInt(accWidth.W)))
+    val y = Output(Vec(outFeatures, SInt(accWidth.W)))
   })
   for (j <- 0 until outFeatures) {
-    val products = Wire(Vec(inFeatures, SInt(32.W)))
+    val products = Wire(Vec(inFeatures, SInt(accWidth.W)))
     for (i <- 0 until inFeatures) {
       products(i) := io.x(i) * io.w(j)(i)
     }
